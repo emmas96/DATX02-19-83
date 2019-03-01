bedae25b963add4bbee3a75b6355d28dd9b2cb65
nicket97 <niclas.o.johansson@telia.com>
2019-03-01 09:51:44 +0100

    woking movetobecon example

---
+++ sc2/Main.py
@@ -0,0 +1,52 @@
+from absl import app
+from pysc2.env import sc2_env
+from pysc2.lib import actions, features, units
+from sc2.SC2TestAgent import SimpleAgent
+
+EPOCHS = 100
+
+
+def main(unused_argv):
+
+    agent = SimpleAgent()
+    points = 0
+    try:
+
+        with sc2_env.SC2Env(
+                map_name="MoveToBeacon",
+                players=[sc2_env.Agent(sc2_env.Race.terran)],
+                agent_interface_format=features.AgentInterfaceFormat(
+                    feature_dimensions=features.Dimensions(screen=15, minimap=15),
+                    use_feature_units=True),
+                step_mul=64,
+                game_steps_per_episode=0,
+                visualize=True) as env:
+
+            agent.setup(env.observation_spec(), env.action_spec())
+            for epoch in range(EPOCHS):
+                agent.reset_game()
+                timesteps = env.reset()
+                agent.reset()
+
+                while True:
+                    if agent.getMemoryLength() > agent.getBatchSize():
+                        agent.train()
+                    step_actions = [agent.step(timesteps[0])]
+                    if timesteps[0].last():
+                        break
+                    timesteps = env.step(step_actions)
+
+                print("epoch: {}/{}, reward: {} Epsilon: {}".format(epoch, EPOCHS, agent.reward, agent.EPSILON))
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    app.run(main)
+
+
+
+
+
+
