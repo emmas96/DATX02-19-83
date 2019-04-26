0cec923aacd0ef5016fdf384c7f3401cbafbfc95
Daniel Willim <daniel@willim.se>
2019-04-26 11:09:51 +0200

    moved stuff

--- misc/plot_MTB_result.py
+++ misc/plot_MTB_result.py
@@ -116,31 +116,42 @@ def calc_avg_win(dir, files):
 
     for file in files:
 
+        print(f"File: {file}")
+
         with open(join(dir, file), 'r') as csv_file:
             data = csv.reader(csv_file, delimiter=',')
 
             for row in data:
                 # Parse data
                 epoch = int(row[0])
-                win = int(row[2])
+
+                print(f"Epoch {epoch} len {len(tot_win)} tot_win {tot_win}")
+
+                if len(tot_win) != 0:
+                    win = tot_win[epoch-1] + float(row[1])
+                else:
+                    print(f"File {file}, was here")
+                    win = float(row[1])
 
                 if first_file:
                     epochs.append(epoch)
                     tot_win.append(win)
                 else:
-                    tot_win[epoch - 1] += win
+                    tot_win[epoch] += win
 
         first_file = False
 
+
     # Calc avg
     avg_win = list(map(lambda x: float(x) / (len(files)), tot_win))
+    print(f"avg_win {avg_win}\n")
     return epochs, avg_win
 
 # Plot one plot with all runs avg
 def plot_frozen_res_avg(dir):
     # Constants
-    nr_to_highlight = 5
-    color = ['b', 'g', 'r', 'c', 'm']
+    nr_to_highlight = 6
+    color = ['b', 'g', 'r', 'c', 'm', 'k']
 
     translations = {}
     test_nr = 1
@@ -265,6 +276,6 @@ def plot_frozen_heatmap(dir):
         plt.show()
 
 
-plot_frozen_res_avg("../Data/Frozen/train")
+plot_frozen_res_avg("../Data/MTB/valid")
 # plot_frozen_res("../Data/Frozen/train")
 # plot_frozen_heatmap("../Data/Frozen/valid")
