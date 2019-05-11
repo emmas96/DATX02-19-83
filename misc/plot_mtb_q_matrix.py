26fda2dc9115cfe0702fca93eb0073a5cc5710eb
Daniel Willim <daniel@willim.se>
2019-05-11 11:23:42 +0200

    work

--- misc/plot_mtb_q_matrix.py
+++ misc/plot_mtb_q_matrix.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import csv
 from os import listdir, makedirs
 from os.path import isfile, join, isdir
 import pandas as pd
 import seaborn as sns
 import pprint
+
+Q = 'Q'
+state = 'state'
 
 
 def save_fig(name, folder):
@@ -15,4 +19,75 @@ def save_fig(name, folder):
         makedirs(path_to_res)
     plt.savefig(join(path_to_res, name))
 
+
+def group_by_run(dir):
+    files = [f for f in listdir(dir) if isfile(join(dir, f))]
+
+    container = {}
+
+    for f in files:
+        run_params = f.split('_')
+        type = str(run_params[1])
+        id = '_'.join(run_params[2:])
+
+        if id in container:
+            container[id][type] = f
+        else:
+            container[id] = {type: f}
+
+    return container
+
+
+def parse_data(dir, file):
+    with open(join(dir, file), 'r') as csv_file:
+        data = csv.reader(csv_file, delimiter=' ')
+
+        matrix = []
+
+        for row in data:
+            parsed = list(map(lambda x: float(x), row))
+            matrix.append(parsed)
+
+        df = pd.DataFrame(matrix)
+        df.sort_index(axis=1, inplace=True)
+        df.sort_index(axis=0, ascending=False, inplace=True)
+
+    return df, matrix
+
+
+def get_xy_coordinates(matrix):
+    highlighted = []
+
+    for y, row in enumerate(matrix):
+        for x, value in enumerate(row):
+            if value == 1:
+                highlighted.append((x, 39 - y))
+
+    return highlighted
+
+
+def mark_cords(ax, cords):
+    for (x, y) in cords:
+        ax.add_patch(Rectangle((x, y), 1, 1, fill=False, edgecolor='green', lw=1))
+
+
+
 def plot_q_matrix(dir):
+
+    all_runs = group_by_run(dir)
+
+    for id, files in all_runs.items():
+        q_df, _ = parse_data(dir, files[Q])
+        state_df, state_matrix = parse_data(dir, files[state])
+
+        ax = sns.heatmap(q_df)
+        beacon_cords = get_xy_coordinates(state_matrix)
+        mark_cords(ax, beacon_cords)
+        plt.show()
+
+        sns.heatmap(state_df, linewidths=.01)
+        plt.show()
+
+
+
+plot_q_matrix('../Data/mtb/state/test')
