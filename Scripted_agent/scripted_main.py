866b336a7dafc53990019d451f9d8f1a823f2827
Daniel Willim <daniel@willim.se>
2019-05-12 21:25:26 +0200

    Work

--- Scripted_agent/scripted_main.py
+++ Scripted_agent/scripted_main.py
@@ -15,7 +15,7 @@ def main(unused_argv):
         with sc2_env.SC2Env(
                 map_name="Simple64",
                 players=[sc2_env.Agent(sc2_env.Race.zerg),
-                         sc2_env.Bot(sc2_env.Race.zerg, sc2_env.Difficulty.very_easy)],
+                         sc2_env.Bot(sc2_env.Race.zerg, sc2_env.Difficulty.easy)],
                 agent_interface_format=features.AgentInterfaceFormat(
                     feature_dimensions=features.Dimensions(screen=84, minimap=64),
                     use_feature_units=True),
